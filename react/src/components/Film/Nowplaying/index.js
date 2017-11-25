9e2d125803e8663c31befed9d0cf6edc75c24f5d
Meiggin <781571871@qq.com>
2017-11-25 14:26:31 +0800

    Add files via upload

---
+++ react/src/components/Film/Nowplaying/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./index.scss";
+
+class Nowplaying extends React.Component{
+	constructor(){
+		super();
+		this.state={
+
+		}
+	}
+
+	render(){
+		return <div>
+			
+			nowplaying
+
+		
+
+		</div>
+	}
+}
+
+export default Nowplaying;
